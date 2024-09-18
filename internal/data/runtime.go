43ea2d6e883fb15862c4a4348211ce994f73ab95
Rupayan Das <rupayan98@gmail.com>
2024-09-18 01:50:12 +0000

    Refactor Movie struct to use custom Runtime type for the runtime field

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -3,7 +3,10 @@ package data
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = fmt.Errorf("invalid runtime format")
 
 type Runtime int32
 
@@ -11,4 +14,25 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
