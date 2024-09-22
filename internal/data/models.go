8714fda490fee14a8b6dac237ca51d2d2ce152a1
Rupayan Das <rupayan98@gmail.com>
2024-09-22 02:38:23 +0000

    Refactor models.go and users.go to include user-related functionality

--- internal/data/models.go
+++ internal/data/models.go
@@ -8,14 +8,17 @@ import (
 var (
 	ErrRecordNotFound = errors.New("record not found")
 	ErrEditConflict   = errors.New("edit conflict")
+	ErrDuplicateEmail = errors.New("duplicate email")
 )
 
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
