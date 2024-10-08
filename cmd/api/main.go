9e3e86cc42d527c2d0cadca3bdcdb6a4a92700cc
Rupayan Das <rupayan98@gmail.com>
2024-10-08 19:23:57 +0000

    Refactor CORS handling and add trusted origins configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -76,6 +80,15 @@ func main() {
 		"smtp-sender",
 		"Greenlight <no-reply@greenlight.rupayan.net>",
 		"SMTP sender",
+	)
+
+	flag.Func(
+		"cors-trusted-origins",
+		"Trusted CORS origins (space separated)",
+		func(val string) error {
+			cfg.cors.trustedOrigins = strings.Fields(val)
+			return nil
+		},
 	)
 
 	flag.Parse()
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -106,7 +106,18 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Vary", "Origin")
+
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					break
+				}
+			}
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
